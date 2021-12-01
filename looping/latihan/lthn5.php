1ac3048dc427387892a3b21ec788ba61be24e86a
arthamevia <yuli45904@gmail.com>
2021-12-01 13:59:16 +0700

    Form Yah

---
+++ looping/latihan/lthn5.php
@@ -0,0 +1,6 @@
+<?php 
+    for($a=10; $a<=54; $a++){
+        if($a % 2==0 )
+        echo "Ini bilangan ganjil: ". $a ."<br>";
+    }
+?>
---
+++ looping/materi/nstd_loop2.php
@@ -0,0 +1,10 @@
+<?php
+
+$i = 0;
+while($i < 4){
+    for($j = 0; $j < 4; $j++){
+        echo "Ini perulangan ke ($i, $j)<br>";
+    }
+
+    $i++;
+}
